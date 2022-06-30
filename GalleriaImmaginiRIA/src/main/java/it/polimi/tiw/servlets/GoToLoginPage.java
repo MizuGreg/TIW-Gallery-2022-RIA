08519e41684442599d91381f08dd00d0d75cebfc
daniPolii <100693976+daniPolii@users.noreply.github.com>
2022-07-01 01:18:15 +0200

    tentativo di risolvere ma niente

--- GalleriaImmaginiRIA/src/main/java/it/polimi/tiw/servlets/GoToLoginPage.java
+++ GalleriaImmaginiRIA/src/main/java/it/polimi/tiw/servlets/GoToLoginPage.java
@@ -21,7 +21,7 @@ public class GoToLoginPage extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	String htmlPath = "/login_page.jsp"; // using HTML extension causes a loop for some damn reason!
+    	String htmlPath = "/login_page.jsp"; 
 		
 		//Checking whether the user is already logged in, in that case send them to the home page
 		HttpSession session = request.getSession(false);
@@ -33,6 +33,11 @@ public class GoToLoginPage extends HttpServlet {
 
         RequestDispatcher requestDispatcher = request.getRequestDispatcher(htmlPath);
         requestDispatcher.include(request, response);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    	doGet(request, response);
     }
 
     @Override
